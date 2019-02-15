7bb405e85b8be12a1135069b7879878a83c8ab53
Manu Ticusan <manuelticusan@yahoo.com>
2019-02-15 21:32:16 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |sentence|
+    sentence.split("").map do |character|
+      ALPHABET.index(character)
+    end
+  end
 end
