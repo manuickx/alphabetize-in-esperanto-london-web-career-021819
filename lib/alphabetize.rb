2359a245afa8e957c347f062fcf33251ac7b4498
Manu Ticusan <manuelticusan@yahoo.com>
2019-02-15 21:31:44 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-    arr.sort_by do |sentence|
+  arr.sort_by do |sentence|
     sentence.split("").map do |character|
       ALPHABET.index(character)
     end
